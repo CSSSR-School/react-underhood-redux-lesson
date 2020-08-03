a934fc4e4bc02a3066bc6e9ec2428cc0676e8391
Максим Водянов <max.post@bk.ru>
2020-08-03 20:14:15 +0300

    added compose

--- redux/redux.js
+++ redux/redux.js
@@ -1,18 +1,32 @@
 module.exports = {
   createStore(reducer, initialState, middlewares) {
+    function compose(...funcs) {
+      if (funcs.length === 0) {
+        return (arg) => arg;
+      }
+
+      if (funcs.length === 1) {
+        return funcs[0];
+      }
+
+      return funcs.reduce((a, b) => (...args) => a(b(...args)));
+    }
     function applyMiddleware(...middlewares) {
-      return (next) => (reducer, initialState) => {
-        const store = next(reducer, initialState);
+      return (createStore) => (reducer, preloadedState, enhancer) => {
+        const store = createStore(reducer, preloadedState, enhancer);
+        let dispatch = store.dispatch;
+        let chain = [];
 
         const middlewareAPI = {
           getState: store.getState,
           dispatch: (action) => dispatch(action),
         };
+        chain = middlewares.map((middleware) => middleware(middlewareAPI));
+        dispatch = compose(...chain)(store.dispatch);
 
-        const chain = middlewares.map((middleware) => middleware(middlewareAPI));
         return {
           ...store,
-          dispatch: [...chain, store.dispatch]
+          dispatch,
         };
       };
     }
