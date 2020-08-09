56dfad08121a072ff66abc101fc12a21725a56c1
Максим Водянов <max.post@bk.ru>
2020-08-09 23:20:05 +0300

    comment fix

--- react-redux/src/Provider/Provider.jsx
+++ react-redux/src/Provider/Provider.jsx
@@ -4,32 +4,30 @@ export const ReactReduxContext = React.createContext(null);
 
 // function Provider({store, children}) {
 //   return <ReactReduxContext.Provider value={store}>
-//       {children}
+//       {children(store)}
 //   </ReactReduxContext.Provider>;
 // }
 
 class Provider extends React.Component {
-    constructor(props) {
-      super(props)
-  
-      const { store } = props
-  
-      this.state = {
-        storeState: store.getState(),
-        store
-      }
-    }
+  constructor(props) {
+    super(props);
 
-  
-    render() {
-      const Context = this.props.context || ReactReduxContext
-  
-      return (
-        <Context.Provider value={this.state}>
-          {this.props.children}
-        </Context.Provider>
-      )
-    }
+    const { store } = props;
+
+    this.state = store;
   }
-  
+
+  render() {
+    const Context = this.props.context || ReactReduxContext;
+
+    return (
+      <Context.Provider value={this.state}>
+        {typeof this.props.children === "function"
+          ? this.props.children(this.store)
+          : this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
 export default Provider;
